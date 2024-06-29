0e9c1fcaa5322b3a3b3749edb24eed421d5a92bc
Supertiger <saawalkhn@googlemail.com>
2024-06-29 14:27:07 +0100

    add background clip and text fill color

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import css from 'css';
 const allowedTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'div', 'img', 'span', 'strong', 'a', "style", "p", "ul", "li", "ol", "table", "thead", "tbody", "tr", "td", "th", "blockquote", "pre", "br"]
 const allowedAttributes = ["href", "src", "color", "style", "class"]
 const allowedCssProperties = [
+  "background-clip",
+  "-webkit-background-clip",
+  "-webkit-text-fill-color",
   "display",
   "position",
   "inset",
